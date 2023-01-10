b36801f8c1afb074d9d48b4d6705d9e63e94c393
ilona <gogiashvili.ilona@gmail.com>
2023-01-10 13:56:22 -0500

    commit tree 2ef1a1ba46b0159a0ff6c88932644d1681bf8ea2

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [searchPlant, setSearchPlant] = useState("")
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(resp => resp.json())
+      .then(setPlants)
+  }, [])
+
+  const addNewPlant = (myPlant) => {
+    setPlants([...plants, myPlant])
+  }
+
+  const updateSearch = (searchInput) => {
+    setSearchPlant(searchInput)
+  }
+
+  let filteredPlants = plants.filter(plant => plant.name.includes(searchPlant))
+  //plant => plant.name.toLowerCase().includes(searchPlant.toLowerCase())
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant} />
+      <Search searchPlant={searchPlant} updateSearch={updateSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-function Search() {
+function Search({searchPlant, updateSearch}) {
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
         type="text"
         id="search"
+        value={searchPlant}
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        onChange={(e)=>updateSearch(e.target.value)}
       />
     </div>
   );
